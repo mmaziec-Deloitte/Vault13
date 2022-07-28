5b407e35788ee0b46cbd637f61e1b070b08fd4dd
mmaziec-deloitte <mmaziec@deloittece.com>
2022-07-28 10:24:04 +0200

    Finish the task (add streams())

--- src/models/Data.java
+++ src/models/Data.java
@@ -8,8 +8,4 @@ import java.util.List;
 public class Data {
     //TO AGATA: I wanted to put all the data and lists in this class, but eventually
     //I didn't know how to do it / didn't have enough time to refactor the Main.java code
-
-
-
-
 }
